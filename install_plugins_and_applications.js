a4740fcf75596eb55b4dbc54c6346220c05ee9a5
danielgayer <80749278+danielgayer@users.noreply.github.com>
2026-02-19 08:55:43 +0100

    Update install_plugins_and_applications.js

--- install_plugins_and_applications.js
+++ install_plugins_and_applications.js
@@ -235,6 +235,7 @@ installPluginsAndApplications([
     'sn_itom_uib_comp', //Service Operations Workspace UI Components
     'sn_apm_mdtl', //Enterprise Modeling and Visualization
     'sn_sow_em_um', //Service Operations Workspace Service Map Monitoring
-    'sn_itom_cloud_svc' //ITOM Cloud Services Core
+    'sn_itom_cloud_svc', //ITOM Cloud Services Core
+    'sn_cloud_ops_ws' //Cloud Discovery Workspace
     
 ]);
